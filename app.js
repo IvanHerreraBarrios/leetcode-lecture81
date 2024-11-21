78494d0828e43b9aabce50df71aacdd8e37daa1c
Ivan Herrera Barrios <ivanherrerabarrios16@gmail.com>
2024-11-21 17:22:21 -0600

    leetcode-lecture81

---
+++ app.js
@@ -0,0 +1,13 @@
+var validateStackSequences = function(pushed, popped) {
+    const stack = []
+    let i = 0
+    for(const el of pushed){
+        stack.push(el)
+        while(stack.length && stack[stack.length - 1] === popped[i]){
+            stack.pop()
+            i++
+        }
+    }
+
+    return pushed.length === i
+};
